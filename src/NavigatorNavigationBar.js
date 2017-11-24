89067aa2201c7f6c08f9f2b3bedd6cadb72327bf
Sunny Luo <sunnylqm@qq.com>
2017-11-24 15:25:45 +0800

    Update NavigatorNavigationBar.js

--- src/NavigatorNavigationBar.js
+++ src/NavigatorNavigationBar.js
@@ -197,7 +197,7 @@ class NavigatorNavigationBar extends React.Component {
       this.props.navigationStyles.Stages.Left;
     rendered = (
       <View
-        key={index}
+        key={componentName + index}
         ref={(ref) => {
           this._components[componentName] = this._components[componentName].set(route, ref);
         }}
